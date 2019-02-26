cef96ecbf81ddf15becbd3f2120ffb264246de88
Ajit Sahu <ajitsahu69@gmail.com>
2019-02-26 13:27:30 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 test -d /usr/local/testDir || mkdir -p /usr/local/testDir
 [ $? -ne 0 ] && echo "NG dir" && exit 10
-chown newuser /usr/local/newDir
+chown cloud_user /usr/local/testDir
 [ $? -ne 0 ] && echo "NO user" && exit 20
 uname -a
 exit 0
