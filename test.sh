98c8c813a6314146516066ff3b9480716bfb16d0
Ajit Sahu <ajitsahu69@gmail.com>
2019-02-26 12:08:22 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -p /usr/local/newDir
+test -d /usr/local/newDir || mkdir -p /usr/local/newDir
 [ $? -ne 0 ] && echo "NG dir" && exit 10
 chown newuser /usr/local/newDir
 [ $? -ne 0 ] && echo "NO user" && exit 20
